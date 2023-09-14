5e20fdf3a2c811b6d6cf4a9607340857f03cf2e6
Pedro Izecksohn <izecksohn@yahoo.com>
2023-09-14 12:22:13 -0300

    Atualizar o RSAsystem.py
    Está versão é mais simples e funciona.

--- RSAsystem.py
+++ RSAsystem.py
@@ -1,27 +1,36 @@
 from os import urandom
+import math
 
 def divisors (number):
     ret=[]
     if number<2:
         return ret
     curdiv = 2
-    while curdiv < number:
+    while (number%2)==0:
+        ret.append(2)
+        number=number//2
+    curdiv=3
+    sqn=number**0.5
+    while curdiv <= sqn:
         if (number%curdiv)==0:
             ret.append(curdiv)
             number = number//curdiv
         else:
-            curdiv += 1
+            curdiv += 2
     ret.append(number)
     return ret
 
 def isPrime(i):
-    if (i<2):
+    if (i<2) or ((i%2)==0):
         return False
-    div=1
-    while div<i:
-        div+=1
+    if (i==2) or (i==3):
+        return True
+    div=3
+    sqipo=int(i**0.5)+1
+    while div<sqipo:
         if (i%div)==0:
             return False
+        div+=2
     return True
 
 def haveCommon (l1, l2):
@@ -29,31 +38,6 @@ def haveCommon (l1, l2):
         if i in l2:
             return True
     return False
-
-def lcm (a, b):
-    if (a==0) or (b==0):
-        return 1
-    if a==b:
-        return a
-    elif a>b:
-        a,b=b,a
-    if (b%a)==0:
-        return b
-    divisor=1
-    ret=1
-    while (a!=1)or(b!=1):
-        divisor+=1
-        while ((a%divisor)==0) and ((b%divisor)==0):
-            a//=divisor
-            b//=divisor
-            ret*=divisor
-        while ((a%divisor)==0):
-            a//=divisor
-            ret*=divisor
-        while ((b%divisor)==0):
-            b//=divisor
-            ret*=divisor
-    return ret
 
 class PrivateKey:
     def __init__(self, n, d):
@@ -84,7 +68,7 @@ class PublicKey:
             raise Exception ("n has "+str(len(l))+" divisors.")
         p=l[0]
         q=l[1]
-        totient=lcm (p-1, q-1)
+        totient=math.lcm (p-1, q-1)
         print ("totient="+str(totient))
         if (self.e<=1) or (self.e>=totient):
             raise Exception ("Invalid e.")
@@ -110,18 +94,18 @@ def genkeys ():
     p=4
     while isPrime(p)==False:
         p=urandom(1)[0]
-        p=(p*256)+urandom(1)[0]
+        #p=(p*256)+urandom(1)[0]
     q=4
     while isPrime(q)==False:
         q=urandom(1)[0]
-        q=(q*256)+urandom(1)[0]
+        #q=(q*256)+urandom(1)[0]
     n=p*q
-    totient=lcm(p-1, q-1)
+    totient=math.lcm(p-1, q-1)
     dt = divisors(totient)
     e=1
     while (e<2) or (e>=totient) or (haveCommon(divisors(e), dt)):
         e=urandom(1)[0]
-        e=(e*256)+urandom(1)[0]
+        #e=(e*256)+urandom(1)[0]
     d=1
     while ((e*d)%totient)!=1:
         d+=1
